864dad751fae0c075b00c9e6d72bd6c25a0b4c33
yagisuke <yagisuke.0209@gmail.com>
2018-05-26 15:57:14 +0900

    Basic grammar of Vue.js

--- src/main.js
+++ src/main.js
@@ -1,71 +1,52 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
 
-Vue.use(Vuex)
+Vue.component('component-item', {
+  template: `
+    <div>template</div>
+  `
+})
 
-const store = new Vuex.Store({
-  state: {
-    count: 0
-  },
-  mutations: {
-    increment(state) {
-      state.count++
-    },
-    decrement(state) {
-      if (state.count === 0) {
-        return
-      }
-
-      state.count--
-    }
-  },
-  actions: {
-    increment(context) {
-      context.commit('increment')
-    },
-    decrement(context) {
-      context.commit('decrement')
-    }
-  }
+Vue.component('component-props-item', {
+  template: `
+    <div>{{ content }}template</div>
+  `,
+  props: ['content']
 })
 
 new Vue({
   el: '#app',
+  data: {
+    count: 0,
+    vShow: false,
+    vIf: true,
+    vForList: [0, 1, 2, 3, 4, 5],
+    vForObject: {
+      one: 1,
+      two: 2,
+      three: 3
+    },
+    vModelText: "default text",
+    vModelTextArea: "default textarea",
+    vModelChecked: false,
+    vModelCheckedList: [],
+    vModelRadio: "FM",
+    vModelSelectBox: "",
+    vModelMultiSelectBox: []
+  },
   computed: {
-    count() {
-      return store.state.count
+    computedOddList() {
+      return this.vForList.filter(num => { return num % 2 !== 0 })
+    },
+    computedEvenList() {
+      return this.vForList.filter(num => { return num % 2 !== 1 })
     }
   },
   methods: {
     increment() {
-      store.dispatch('increment')
+      this.count++
     },
     decrement() {
-      store.dispatch('decrement')
+      this.count--
     }
-  },
-  beforeCreate: () => {
-    console.log('ライフサイクルフック: beforeCreate')
-  },
-  created: () => {
-    console.log('ライフサイクルフック: created')
-  },
-  beforeMount: () => {
-    console.log('ライフサイクルフック: beforeMount')
-  },
-  mounted: () => {
-    console.log('ライフサイクルフック: mounted')
-  },
-  beforeUpdate: () => {
-    console.log('ライフサイクルフック: beforeUpdate')
-  },
-  updated: () => {
-    console.log('ライフサイクルフック: updated')
-  },
-  beforeDestroy: () => {
-    console.log('ライフサイクルフック: beforeDestroy')
-  },
-  destroyed: () => {
-    console.log('ライフサイクルフック: destroyed')
   }
 })
